9b6181f868b14b72a576eb82f59d3bcbf6c1ee61
Alec Asperslag <asperslaga@gmail.com>
2020-08-08 14:17:23 -0700

    More test scene, now with flow charts
    run into the orb

---
+++ LoveSnails/Assets/Scripts/StartDialogue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+using Fungus;
+
+public class StartDialogue : MonoBehaviour
+{
+    public string SceneNameToOpen;
+    public Flowchart Dialogueflowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Dialogueflowchart.ExecuteBlock("Start");
+        //SceneManager.LoadScene(SceneNameToOpen);
+    }
+}
