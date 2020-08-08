9bb2bc5d17c5de2a6366aaf53b7fb92abe6c62e8
Alec Asperslag <asperslaga@gmail.com>
2020-08-08 12:45:59 -0700

    Alec Snail
    Added a real simple movement/character script and some folders

---
+++ LoveSnails/Assets/Scripts/SnailMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailMover : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public GameObject SnailModel;
+
+    Rigidbody rigidbody;
+    private Vector3 inputVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = Vector3.zero;
+
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.z = Input.GetAxis("Vertical");
+
+        if (inputVector != Vector3.zero)
+        {
+            SnailModel.transform.forward = inputVector;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + inputVector * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
