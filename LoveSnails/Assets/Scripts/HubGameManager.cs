aa39ca8168cdbb4545cffa2cc6873fe708d45dde
Alec Asperslag <asperslaga@gmail.com>
2020-08-08 20:55:17 -0700

    Added Timer Code to Hub Game Manager
    It doesn't do anything yet, but will trigger a method

--- LoveSnails/Assets/Scripts/HubGameManager.cs
+++ LoveSnails/Assets/Scripts/HubGameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public enum ConvoStatus { InHub, InConvo };
 
@@ -9,11 +10,19 @@ public class HubGameManager : MonoBehaviour
     //lazy singleton
     public static HubGameManager Instance;
 
+    //CAMERA/PLAYER
     public Camera ThirdPersonCam;
     public Camera FlowchartCam;
     public GameObject PlayerObject;
 
-    private ConvoStatus ConversationStatus;
+    //TIMER
+    public float TimerInSeconds = 480; //8 minutes default
+    public float TimeRemaining = 0;
+    public UnityEvent TimeIsUp;
+
+    bool IsTimerActive = false;
+
+    //private ConvoStatus ConversationStatus;
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +31,55 @@ public class HubGameManager : MonoBehaviour
         {
             Instance = this;
         }
-        ConversationStatus = ConvoStatus.InHub;
+        //ConversationStatus = ConvoStatus.InHub;
     }
 
-    public void SwapCamera()
+    public void TimerStart()
     {
-        if(ConversationStatus == ConvoStatus.InHub)
+        TimeRemaining = TimerInSeconds;
+    }
+
+    public void PauseTimer(bool pauseValue)
+    {
+        IsTimerActive = pauseValue;
+        //Any other pause code goes here. 
+    }
+
+    private void Update()
+    {
+        if (!IsTimerActive)
         {
-            PlayerObject.SetActive(false);
-            ThirdPersonCam.gameObject.SetActive(false);
-            FlowchartCam.gameObject.SetActive(true);
-            ConversationStatus = ConvoStatus.InConvo;
-        }
-        else if (ConversationStatus == ConvoStatus.InConvo)
-        {
-            PlayerObject.SetActive(true);
-            FlowchartCam.gameObject.SetActive(false);
-            ThirdPersonCam.gameObject.SetActive(true);
-            ConversationStatus = ConvoStatus.InHub;
+            TimeRemaining -= Time.deltaTime;
+            if(TimeRemaining <= 0)
+            {
+                IsTimerActive = false;
+                TimerEndCode();
+            }
         }
     }
+
+    public void TimerEndCode()
+    {
+        //PUT WHATEVER NEEDS TO TRIGGER ON TIMER END HERE
+        //MADE AN EVENT IN CASE WE NEED TO TRIGGER MORE STUFF
+        TimeIsUp.Invoke();
+    }
+
+    //public void SwapCamera()
+    //{
+    //    if(ConversationStatus == ConvoStatus.InHub)
+    //    {
+    //        PlayerObject.SetActive(false);
+    //        ThirdPersonCam.gameObject.SetActive(false);
+    //        FlowchartCam.gameObject.SetActive(true);
+    //        ConversationStatus = ConvoStatus.InConvo;
+    //    }
+    //    else if (ConversationStatus == ConvoStatus.InConvo)
+    //    {
+    //        PlayerObject.SetActive(true);
+    //        FlowchartCam.gameObject.SetActive(false);
+    //        ThirdPersonCam.gameObject.SetActive(true);
+    //        ConversationStatus = ConvoStatus.InHub;
+    //    }
+    //}
 }
